936e9592fa36c9e0c8077bd22c3c9534dc1c334a
AmiN4iK03 <amin4ik.2003@gmail.com>
2020-05-22 17:38:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.07e359f3ff5b50125873f49ba844dfb7.js"
+  "./precache-manifest.92117beb8538cc56115a17ed4d9d5096.js"
 );
 
 self.addEventListener('message', (event) => {
